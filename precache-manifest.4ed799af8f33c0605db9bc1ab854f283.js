017967b34496af258af256588e6724d92fcde186
DmJSCourse <dimonua@gmail.com>
2020-09-22 03:25:19 +0300

    Updates

---
+++ precache-manifest.4ed799af8f33c0605db9bc1ab854f283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d710b7993e3f083e1ed09074c3bf78",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "078aa2e692c4f1e5894b",
+    "url": "/react_dynamic-list-of-posts/static/css/main.aafd6d15.chunk.css"
+  },
+  {
+    "revision": "ad69bceed2e46a0cbb7a",
+    "url": "/react_dynamic-list-of-posts/static/js/2.9c29ece5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_dynamic-list-of-posts/static/js/2.9c29ece5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078aa2e692c4f1e5894b",
+    "url": "/react_dynamic-list-of-posts/static/js/main.6b45ea45.chunk.js"
+  },
+  {
+    "revision": "618358863681a9841d76",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime-main.d29b7207.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "/react_dynamic-list-of-posts/precache-manifest.4ed799af8f33c0605db9bc1ab854f283.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_dynamic-list-of-posts/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
